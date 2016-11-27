c7819d5a1f49968ccdff52ac5b025fac18e56205
Igor Grebenkov <igor.grebenkov@gmail.com>
2016-11-26 20:23:04 -0500

    added instructions activity

---
+++ app/src/main/java/uottawa/ca/cookhelper/InstructionsActivity.java
@@ -0,0 +1,13 @@
+package uottawa.ca.cookhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
